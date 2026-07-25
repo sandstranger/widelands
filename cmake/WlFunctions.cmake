798fa3c4fdb1e735182e22b1f5cd074017e9db0f
sandstranger <sandstranger@gmail.com>
2026-07-25 23:34:58 +0400

    added saf support

--- cmake/WlFunctions.cmake
+++ cmake/WlFunctions.cmake
@@ -224,7 +224,7 @@ endif ()
 
     if (ANDROID)
       wl_include_system_directories(${NAME} ${GLOB_INCLUDE_DIR})
-      target_link_libraries(${NAME} ${GLOB_LIBRARY} log AndroidGraphicsLayer)
+      target_link_libraries(${NAME} ${SAFFAL_LIBRARY} ${GLOB_LIBRARY} log AndroidGraphicsLayer)
     endif ()
 
   foreach(DEPENDENCY ${ARG_DEPENDS})
